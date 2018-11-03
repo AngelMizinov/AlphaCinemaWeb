e8b222b00be6de34b0852c4dbaa3123d44a0c954
Angel Mizinov <angelmizinov@abv.bg>
2018-11-04 00:17:40 +0200

    commit after cached

---
+++ AlphaCinemaData/Configurations/OpenHourConfiguration.cs
@@ -0,0 +1,36 @@
+﻿using AlphaCinemaData.Models;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace AlphaCinemaData.Configurations
+{
+	public class OpenHourConfiguration : IEntityTypeConfiguration<OpenHour>
+	{
+		public void Configure(EntityTypeBuilder<OpenHour> builder)
+		{
+			builder
+				.HasKey(oh => oh.Id);
+
+			builder
+				.Property(opHour => opHour.Hours)
+				.IsRequired(true);
+
+			builder
+				.HasIndex(opHour => opHour.Hours)
+				.IsUnique(true);
+
+			builder
+				.Property(opHour => opHour.Minutes)
+				.IsRequired(true);
+
+			builder
+				.HasIndex(opHour => opHour.Minutes)
+				.IsUnique(true);
+
+			builder
+				.HasMany(oh => oh.Projections)
+				.WithOne(p => p.OpenHour)
+				.HasForeignKey(p => p.OpenHourId);
+		}
+	}
+}
---
+++ AlphaCinemaData/Models/Genre.cs
@@ -0,0 +1,12 @@
+﻿using AlphaCinemaData.Models.Abstract;
+using AlphaCinemaData.Models.Associative;
+using System.Collections.Generic;
+
+namespace AlphaCinemaData.Models
+{
+    public class Genre : Entity
+    {
+		public string Name { get; set; }
+		public ICollection<MovieGenre> MoviesGenres { get; set; }
+    }
+}
