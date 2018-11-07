edb2c85f9e4087aac03841cbc83a8f2676301442
Krasimir Etov <4341025+KrasimirEtov@users.noreply.github.com>
2018-11-07 02:30:15 +0200

    Scroll to top button, changed admin panel index, admin dropdown menu changes, implemented add-remove a dmin roles, some ui improvements

---
+++ AlphaCinemaWeb/Areas/Administration/Models/ManageRoleViewModels/UserViewModel.cs
@@ -0,0 +1,32 @@
+﻿using AlphaCinemaData.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace AlphaCinemaWeb.Areas.Administration.Models.ManageRoleViewModels
+{
+	public class UserViewModel
+	{
+		public UserViewModel()
+		{
+
+		}
+
+		public UserViewModel(User user)
+		{
+			Username = user.UserName;
+			FirstName = user.FirstName;
+			LastName = user.LastName;
+			Age = user.Age;
+			this.Id = user.Id;
+		}
+
+		public string Id { get; set; }
+		public string Username { get; set; }
+		public string FirstName { get; set; }
+		public string LastName { get; set; }
+		public int Age { get; set; }
+		public bool IsAdmin { get; set; }
+	}
+}
